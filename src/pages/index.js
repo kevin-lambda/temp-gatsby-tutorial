f13415a9eee314e36a505de78a4ca755016c1593
Kevin Lam <kevin.quoct.lam@gmail.com>
2021-10-16 12:13:28 -0700

    try 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/SEO"
 export default function Home() {
   return (
     <Layout>
-      <SEO title="Home " />
+      <SEO title="test update title Home " />
       <main className="page">
         <header className="hero">
           <StaticImage
